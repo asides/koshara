60672faa08d793b359ea2f67c1c74fab00eb38b6
asides <yaroslav.str@gmail.com>
2013-08-10 13:52:24 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koshara::Application.config.secret_token = '854130c4cac9fa962297efba1a06c48809e3d83136399aa49bd5f152c40dc13e5b18116b8c7c12dd5291f6bab9fdfb26bf4bf47a77612511ce5fd65ed3c58a33'
